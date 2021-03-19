d0945bec066c26761ae69de28c6ca37a3ffa6940
HAFDIAHMED <ahafdi62@gmail.com>
2021-03-19 15:09:31 +0100

    http depency

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 
 Future<Post> fetchPost() async {
   final response =
-      await http.get('https://jsonplaceholder.typicode.com/posts/1');
+      await http.get('https://jsonplaceholder.typicode.com/posts/');
 
   if (response.statusCode == 200) {
     // If the call to the server was successful, parse the JSON
